1ad7c5c66b173d67872f60af699945efe728b84c
Himanshu <72544756+thecyberjerry@users.noreply.github.com>
2022-12-23 21:09:33 +0530

    Delete fetch.js

--- fetch.js
+++
@@ -1,8 +0,0 @@
-let fetchURL = 'https://github.com/thecyberjerry/Shttps://github.com/thecyberjerry/Subli/blob/main/shot.png';
-  let image = name.map((picName) => {
-    return picName
-  })
-
-  fetch(fetchURL + image)
-  .then(response => response.json())
-  .then(images => console.log(fetchURL + images));
